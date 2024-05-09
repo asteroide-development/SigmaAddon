7e93f282c1ef1d402cf2b0f13c7aa348ba164c37
knew.stm <justinnowakjustinnowak@gmail.com>
2024-05-09 14:18:34 +0200

    so sigma!!

--- src/main/java/com/example/addon/modules/ModuleExample.java
+++ src/main/java/com/example/addon/modules/ModuleExample.java
@@ -2,9 +2,18 @@ package com.example.addon.modules;
 
 import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
+import meteordevelopment.orbit.EventHandler;
 
 public class ModuleExample extends Module {
     public ModuleExample() {
         super(Addon.CATEGORY, "example", "An example module in a custom category.");
+    }
+    @EventHandler
+    private void onTick(TickEvent.Pre event){
+        info("Sigma");
     }
 }
